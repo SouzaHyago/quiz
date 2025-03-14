07c5f0423c7cad9d39de907447ff749cc5a50f02
Hyago Souza <hyago.souza0103@gmail.com>
2025-03-14 14:54:59 -0300

    making the quiz random

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import "./quiz.css"
 
 
 function shuffleArray(array) {
-  const newArray = array.slice(); // cria uma cópia
+  const newArray = array.slice();
   for (let i = newArray.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
@@ -264,8 +264,8 @@ export default function Home() {
 
 
   function restart(){
+    shuffleArray(quiz)
     setScore(0);
-    console.log(playing)
     
   }
 
